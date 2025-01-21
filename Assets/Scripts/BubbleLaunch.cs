a373834b95c2ba046038ffb056e684d53fbd3d5e
Cambob03 <cameron.best@outlook.com>
2025-01-21 18:55:15 +0000

    Fixed Jump Issue

--- Assets/Scripts/BubbleLaunch.cs
+++ Assets/Scripts/BubbleLaunch.cs
@@ -10,17 +10,24 @@ public class BubbleLaunch : MonoBehaviour
     private Dictionary<Color, (Vector3 direction, float force)> colorLaunchProperties;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     // Start is called before the first frame update
 =======
+=======
+>>>>>>> Stashed changes
     private float verticalLaunchPower = 50f;
     private float horizontalLaunchPower = 1000f;
     private float diagonalLaunchPower = 100f;
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
     void Start()
     {
         colorLaunchProperties = new Dictionary<Color, (Vector3 direction, float force)>
        {
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
            { Color.red, (Vector3.left, 10f)},
            { Color.yellow, (Vector3.up, 10f)},
@@ -40,6 +47,16 @@ public class BubbleLaunch : MonoBehaviour
            { new Color (1f, 0.647f, 0), (new Vector3(-1f, 1f, 0), diagonalLaunchPower)} //oramge
        };
 
+=======
+           { Color.red, (new Vector3(-1f, 0, 0), horizontalLaunchPower)},
+           { new Color(1, 0.92f, 0.016f), (new Vector3(0, 1f, 0), verticalLaunchPower)},
+           { Color.blue, (new Vector3(1f, 0, 0), horizontalLaunchPower)},
+           { Color.green, (new Vector3(1f, 1f, 0),diagonalLaunchPower)},
+           { new Color (0.5f, 0, 0.5f), (new Vector3(0, -1f, 0), verticalLaunchPower)}, //purple
+           { new Color (1f, 0.647f, 0), (new Vector3(-1f, 1f, 0), diagonalLaunchPower)} //oramge
+       };
+
+>>>>>>> Stashed changes
         BubbleMerge = GetComponent<BubbleMerge>();
 
         Vector3 RespawnPosition = gameObject.transform.position;
@@ -58,6 +75,18 @@ public class BubbleLaunch : MonoBehaviour
             {
                 Vector3 launchDirection = launchProperties.direction;
                 float launchForce;
+<<<<<<< Updated upstream
+=======
+
+                if (BubbleMerge.hasMerged == false)
+                {
+                    launchForce = launchProperties.force;
+                }
+                else
+                {
+                    launchForce = launchProperties.force * 2;
+                }
+>>>>>>> Stashed changes
 
                 if (BubbleMerge.hasMerged == false)
                 {
